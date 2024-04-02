0f19a7c5c42d769b5e6133dfe98fb5519428981c
Jessica Tell <hello.jessicatell@gmail.com>
2024-04-02 19:06:28 +0200

    wip with stripe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 const App = () => {
   const handlePayment = async () => {
-    const response = await fetch(
-      "http://localhost:3000/payments/create-checkout-session",
-      {
-        method: "POST",
-      }
-    );
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await fetch(
+        "http://localhost:3000/payments/create-checkout-session",
+        {
+          method: "POST",
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Failed to fetch:", error);
+    }
   };
 
   return (
--- server/stripe/stripe.controller.js
+++ server/stripe/stripe.controller.js
@@ -1,5 +1,22 @@
-const createCheckoutSession = (req, res) => {
-  res.json("Hello");
+const initStripe = require("../stripe");
+
+const createCheckoutSession = async (req, res) => {
+  const stripe = initStripe();
+
+  const session = await stripe.checkout.sessions.create({
+    mode: "payment",
+    line_items: [
+      //vår backend, stripe kommer behöva objekt som ser ut såhär - tänk på det när bygger client så den får bra info från början
+      {
+        price: "price_1P17pv05kEsouJvUgx9bSwaT",
+        quantity: 1,
+      },
+    ],
+    success_url: "http://localhost:5173/confirmation",
+    cancel_url: "http://localhost:5173/cancel",
+  });
+
+  res.status(200).json({ url: session.url });
 };
 
 module.exports = { createCheckoutSession };

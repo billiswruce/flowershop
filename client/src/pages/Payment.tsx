9896354689547c2bbafb0af7107b678567b33805
Jessica Tell <hello.jessicatell@gmail.com>
2024-04-08 22:40:52 +0200

    btn-styling

--- client/src/pages/Payment.tsx
+++ client/src/pages/Payment.tsx
@@ -1,36 +1,36 @@
-const Payment = () => {
-  const handlePayment = async () => {
-    const response = await fetch(
-      "http://localhost:3000/payments/create-checkout-session",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify([
-          {
-            product: "price_1P17xu05kEsouJvU9D264jnm",
-            quantity: 2,
-          },
-          {
-            product: "price_1P17pv05kEsouJvUgx9bSwaT",
-            quantity: 1,
-          },
-        ]),
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    localStorage.setItem("sessionId", JSON.stringify(data.sessionId));
+// const Payment = () => {
+//   const handlePayment = async () => {
+//     const response = await fetch(
+//       "http://localhost:3000/payments/create-checkout-session",
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify([
+//           {
+//             product: "price_1P17xu05kEsouJvU9D264jnm",
+//             quantity: 2,
+//           },
+//           {
+//             product: "price_1P17pv05kEsouJvUgx9bSwaT",
+//             quantity: 1,
+//           },
+//         ]),
+//       }
+//     );
+//     const data = await response.json();
+//     console.log(data);
+//     localStorage.setItem("sessionId", JSON.stringify(data.sessionId));
 
-    window.location = data.url;
-  };
+//     window.location = data.url;
+//   };
 
-  return (
-    <div>
-      <button onClick={handlePayment}>Let's pay!</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <button onClick={handlePayment}>Let's pay!</button>
+//     </div>
+//   );
+// };
 
-export default Payment;
+// export default Payment;

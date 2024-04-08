a00d811d431c417a9bc28f5a1b4a58d0ca44e54a
Jessica Tell <hello.jessicatell@gmail.com>
2024-04-08 20:58:41 +0200

    modal done

---
+++ client/src/components/CartModal.tsx
@@ -0,0 +1,28 @@
+import { useCart } from "../context/CartContext";
+import "../styles/Header.css";
+
+const CartModal = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  const { cart } = useCart();
+
+  return (
+    <div className={isOpen ? "cart-modal" : "cart-modal cart-modal-hidden"}>
+      <button onClick={onClose}>Close</button>
+      <h2>Your Cart</h2>
+      <ul>
+        {cart.map((item) => (
+          <li key={item.product.id}>
+            {item.product.name} - Antal: {item.quantity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CartModal;
